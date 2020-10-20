00df77c50e16bc3eae86d0fe2e592bc1debaf269
NicoPaez <nicopaez@gmail.com>
2020-10-19 21:57:03 -0300

    Port mapping added

--- ejecutar_docker.sh
+++ ejecutar_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
- docker run -it --rm -v$PWD:/workspace registry.gitlab.com/fiuba-memo2/imagen-para-katas:latest /bin/bash
+ docker run -it --rm -p4567:4567 -v$PWD:/workspace registry.gitlab.com/fiuba-memo2/imagen-para-katas:latest /bin/bash
