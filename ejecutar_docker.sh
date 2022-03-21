b05838d271f7cc84fbbd061e71805250eec8330b
NicoPaez <nicoapez@gmail.com>
2022-03-21 16:37:04 -0300

    Update to ruby 2.7.5

--- ejecutar_docker.sh
+++ ejecutar_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
- docker run -it --rm -p4567:4567 -v$PWD:/workspace registry.gitlab.com/fiuba-memo2/imagen-para-katas:latest /bin/bash
+ docker run -it --rm -p4567:4567 -v$PWD:/workspace registry.gitlab.com/fiuba-memo2/imagen-para-katas:1.3.0 /bin/bash
