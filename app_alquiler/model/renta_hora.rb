11a73e466388224b0d15358a65e6dd74a9f688b4
Juan <jroussilian@fi.uba.ar>
2023-03-19 15:44:31 -0300

    Pasan los tests de renta_hora

--- app_alquiler/model/renta_hora.rb
+++ app_alquiler/model/renta_hora.rb
@@ -1,2 +1,14 @@
 class RentaHora
+  def initialize(horas)
+    @horas = horas
+  end
+
+  def calcular_importe(fecha_inicial, fecha_entrega)
+    d_inicial = Date.parse(fecha_inicial)
+    d_acordada = d_inicial.next_day(@horas / 24)
+    d_entrega = Date.parse(fecha_entrega)
+    return @horas * 99 unless d_entrega > d_acordada
+
+    @horas * 99 * 2
+  end
 end
--- app_alquiler/spec/renta_hora_spec.rb
+++ app_alquiler/spec/renta_hora_spec.rb
@@ -1,22 +1,23 @@
 require 'spec_helper'
 
 describe 'RentaHora' do
-=begin
-  it 'renta sin recargo por 1 hora calcula bien el importe' do
+
+  it 'renta sin recargo por 1 hora debe dar un importe de 99' do
     renta = RentaHora.new(1)
-    importe = renta.calcular_importe('20231903','20231903')
+    importe = renta.calcular_importe('20230319','20230319')
     expect(importe).to eq 99
 
   end
-  it 'renta sin recargo por 49 horas calcula bien el importe' do
+  it 'renta sin recargo por 49 horas debe dar un importe de 4950' do
     renta = RentaHora.new(50)
-    importe = renta.calcular_importe('20231903','20232203')
+    importe = renta.calcular_importe('20230319','20230321')
     expect(importe).to eq 4950
   end
+
   it 'renta con recargo por 25 horas calcula bien el importe' do
     renta = RentaHora.new(25)
-    importe = renta.calcular_importe('20231903','20232204')
+    importe = renta.calcular_importe('20230319','20230422')
     expect(importe).to eq 4950
   end
-=end
+
 end
