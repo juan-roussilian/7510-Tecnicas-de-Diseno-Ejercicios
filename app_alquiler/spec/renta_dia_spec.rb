8c9be0e1717aadfd17d59deee2a7a048321008c5
Juan <jroussilian@fi.uba.ar>
2023-03-19 15:30:00 -0300

    corrijo nombre de tests de renta dia

--- app_alquiler/spec/renta_dia_spec.rb
+++ app_alquiler/spec/renta_dia_spec.rb
@@ -8,13 +8,13 @@ describe 'RentaDia' do
     expect(importe).to eq 2001
 
   end
-  it 'renta sin recargo por 20 dias calcula bien el importe' do
+  it 'renta sin recargo por 20 dias dabe dar importe de 40020' do
     renta = RentaDia.new(20)
     importe = renta.calcular_importe('20230309','20230329')
     expect(importe).to eq 40020
   end
 
-  it 'renta con recargo por 3 dias calcula bien el importe' do
+  it 'renta con recargo por 3 dias debe dar importe de 12006' do
     renta = RentaDia.new(3)
     importe = renta.calcular_importe('20230319','20230425')
     expect(importe).to eq 12006
