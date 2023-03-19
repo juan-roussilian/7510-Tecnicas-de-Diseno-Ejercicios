34b944386387272f5b9b9aa50f2ab159b1db489b
Juan <jroussilian@fi.uba.ar>
2023-03-19 13:25:44 -0300

    Corrijo los tests de cuit empresarial e implemento logica para que pasen

--- app_alquiler/model/cuit_empresarial.rb
+++ app_alquiler/model/cuit_empresarial.rb
@@ -1,2 +1,5 @@
 class CuitEmpresarial
+  def aplicar_descuento(importe)
+    importe - importe / 100 * 5
+  end
 end
--- app_alquiler/spec/cuit_empresarial_spec.rb
+++ app_alquiler/spec/cuit_empresarial_spec.rb
@@ -1,11 +1,8 @@
 require 'spec_helper'
 
 describe 'CuitEmpresarial' do
-=begin
-  it 'aplicar descuento en cuit empresarial descuenta correctamente' do
-    porcentaje_descuento = 5
-    importe_descontado = CuitParticular.new.aplicar_descuento(500)
-    expect(importe_descontado).to eq (500 - 500%100*porcentaje_descuento)
+  it 'descontar con cuit empresarial sobre 500 da 475' do
+    importe_descontado = CuitEmpresarial.new.aplicar_descuento(500)
+    expect(importe_descontado).to eq 475
   end
-=end
 end
