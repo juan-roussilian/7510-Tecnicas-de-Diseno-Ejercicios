80fd05f741df827654e0e442ed07821dbb2e4e77
Juan <jroussilian@fi.uba.ar>
2023-03-19 13:50:52 -0300

    pasan los tests de rentaKm

--- app_alquiler/spec/renta_km_spec.rb
+++ app_alquiler/spec/renta_km_spec.rb
@@ -1,22 +1,24 @@
 require 'spec_helper'
 
 describe 'RentaKm' do
-=begin
-  it 'renta sin recargo por 1 km calcula bien el importe' do
+
+  it 'renta sin recargo por 1 km debe dar importe de 110' do
     renta = RentaKm.new(1)
-    importe = renta.calcular_importe('20231903','20231903')
+    importe = renta.calcular_importe('20230319','20230319')
     expect(importe).to eq 110
 
   end
-  it 'renta sin recargo por 100 km calcula bien el importe' do
+
+  it 'renta sin recargo por 100 km debe dar importe de 1100' do
     renta = RentaKm.new(100)
-    importe = renta.calcular_importe('20231903','20231903')
+    importe = renta.calcular_importe('20230319','20230319')
     expect(importe).to eq 1100
   end
-  it 'renta con recargo por 100 km calcula bien el importe' do
+
+  it 'renta con recargo por 100 km debe dar importe de 2200' do
     renta = RentaKm.new(100)
-    importe = renta.calcular_importe('20231903','20231904')
+    importe = renta.calcular_importe('20230303','20230304')
     expect(importe).to eq 2200
   end
-=end
+
 end
