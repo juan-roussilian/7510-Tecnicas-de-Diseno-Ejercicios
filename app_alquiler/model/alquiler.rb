26d2e9150fab3cff547c6dcf603563380d436382
Juan <jroussilian@fi.uba.ar>
2023-03-19 22:12:22 -0300

    Pasan los tests de alquiler

--- app_alquiler/model/alquiler.rb
+++ app_alquiler/model/alquiler.rb
@@ -1,2 +1,30 @@
 class Alquiler
+  def initialize(fecha_inicio, fecha_entrega, cuit, tipo_alquiler, parametro_alquiler)
+    @fecha_inicio = fecha_inicio
+    @fecha_entrega = fecha_entrega
+
+    @cuit = if cuit[0..1] == '26'
+              CuitEmpresarial.new
+            else
+              CuitParticular.new
+            end
+
+    @renta = case tipo_alquiler
+             when 'd'
+               RentaDia.new(parametro_alquiler.to_i)
+             when 'h'
+               RentaHora.new(parametro_alquiler.to_i)
+             else
+               RentaKm.new(parametro_alquiler.to_i)
+             end
+  end
+
+  def importe_total
+    importe = @cuit.aplicar_descuento(@renta.calcular_importe(@fecha_inicio, @fecha_entrega))
+    format('%05.1f', importe)
+  end
+
+  def ganancia(importe)
+    format('%05.1f', importe * 0.45)
+  end
 end
--- app_alquiler/spec/alquiler_spec.rb
+++ app_alquiler/spec/alquiler_spec.rb
@@ -1,37 +1,36 @@
 require 'spec_helper'
 
 describe 'Alquiler' do
-=begin
+
   it 'Alquiler particular de 3 dias sin recargo devuelve importe y ganancia correcta' do
-    alquiler = Alquiler.new('20230101','20230401','20421937649','d','3')
+    alquiler = Alquiler.new('20230101','20230104','20421937649','d','3')
     importe = alquiler.importe_total
-    ganancia = alquiler.calcular_ganancia(importe)
-    expect(importe).to eq '297.0'
-    expect(ganancia).to eq '133.7'
+    ganancia = alquiler.ganancia(importe.to_f)
+    expect(importe).to eq '6003.0'
+    expect(ganancia).to eq '2701.4'
   end
 
   it 'Alquiler empresarial de 5 dias con recargo devuelve importe y ganancia correcta' do
-    alquiler = Alquiler.new('20230101','20232001','26421937649','d','5')
+    alquiler = Alquiler.new('20230101','20230120','26421937649','d','5')
     importe = alquiler.importe_total
-    ganancia = alquiler.calcular_ganancia(importe)
-    expect(importe).to eq '949.5'
-    expect(ganancia).to eq '423.2'
+    ganancia = alquiler.ganancia(importe.to_f)
+    expect(importe).to eq '19009.5'
+    expect(ganancia).to eq '8554.3'
   end
 
   it 'Alquiler empresarial de 20000km sin recargo devuelve importe y ganancia correcta' do
     alquiler = Alquiler.new('20230101','20230101','26421937649','k','20000')
     importe = alquiler.importe_total
-    ganancia = alquiler.calcular_ganancia(importe)
-    expect(importe).to eq '190100.0'
-    expect(ganancia).to eq '85600.0'
+    ganancia = alquiler.ganancia(importe.to_f)
+    expect(importe).to eq '190095.0'
+    expect(ganancia).to eq '85542.8'
   end
 
   it 'Alquiler particular de 29 horas sin recargo devuelve importe y ganancia correcta' do
-    alquiler = Alquiler.new('20230101','20230201','20421937649','h','29')
+    alquiler = Alquiler.new('20230101','20230102','20421937649','h','29')
     importe = alquiler.importe_total
-    ganancia = alquiler.calcular_ganancia(importe)
+    ganancia = alquiler.ganancia(importe.to_f)
     expect(importe).to eq '2871.0'
     expect(ganancia).to eq '1292.0'
   end
-=end
 end
