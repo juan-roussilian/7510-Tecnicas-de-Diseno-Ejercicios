31a4f2e4b24e280c924a9b7e23aa3b80bdf6b9cd
Juan <jroussilian@fi.uba.ar>
2023-03-26 18:46:21 -0300

    Agrego otro test unitario para propiedad comercio para mayor robustez

--- app_alarma/spec/comericio_spec.rb
+++ app_alarma/spec/comericio_spec.rb
@@ -8,6 +8,16 @@ describe 'Comercio' do
     comercio = Comercio.new(sensor_apertura, sensor_movimiento, sensor_camara)
     expect(comercio.estado).to eq 'ASEGURADA'
   end
+
+
+  it 'Comercio con movimiento 10 activo y camara activo esta asegurado' do
+    sensor_apertura = SensorSimple.new(0)
+    sensor_movimiento = SensorMovimiento.new(10)
+    sensor_camara = SensorSimple.new(1)
+    comercio = Comercio.new(sensor_apertura, sensor_movimiento, sensor_camara)
+    expect(comercio.estado).to eq 'ASEGURADA'
+  end
+
   it 'Comercio con sensor de apertura activo y camara activo esta comprometido' do
     sensor_apertura = SensorSimple.new(1)
     sensor_movimiento = SensorMovimiento.new(0)
