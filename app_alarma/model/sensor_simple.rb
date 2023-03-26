e90efa4fe449a6fdc29f1550c73a301efc1aa8eb
Juan <jroussilian@fi.uba.ar>
2023-03-26 19:36:49 -0300

    Agrego que sensor simple esta descompuesto si se quiere inicializar con algun valor distinto a 0 o 1

--- app_alarma/model/sensor_simple.rb
+++ app_alarma/model/sensor_simple.rb
@@ -1,7 +1,13 @@
+require_relative '../exceptions/sensor_descompuesto'
 class SensorSimple
   VALOR_ACTIVIDAD = 1
-  def initialize(valor)
-    @valor_numerico = valor
+  VALOR_INACTIVIDAD = 0
+  def initialize(valor_numerico)
+    if valor_numerico != VALOR_ACTIVIDAD && valor_numerico != VALOR_INACTIVIDAD
+      raise SensorDescompuesto
+    end
+
+    @valor_numerico = valor_numerico
   end
 
   def activo
--- app_alarma/spec/sensor_simple_spec.rb
+++ app_alarma/spec/sensor_simple_spec.rb
@@ -11,4 +11,9 @@ describe 'SensorSimple' do
     sensor = SensorSimple.new(1)
     expect(sensor.activo).to eq true
   end
+
+  it 'sensor simple con valor distinto a 0 o 1 esta descompuesto' do
+    expect { SensorSimple.new(2) }.to raise_error(SensorDescompuesto)
+  end
+
 end
