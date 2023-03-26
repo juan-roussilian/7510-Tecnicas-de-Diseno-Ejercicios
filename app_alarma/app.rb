a9bafbbd61ed1b56e40ebd6d905ee635a36461bc
Juan <jroussilian@fi.uba.ar>
2023-03-26 18:31:32 -0300

    Refactor: cambio nombre de sensor puerta a sensor apertura

--- app_alarma/app.rb
+++ app_alarma/app.rb
@@ -2,9 +2,9 @@ require_relative './model/sensor_simple'
 require_relative './model/sensor_movimiento'
 require_relative './model/departamento'
 
-sensor_puerta = SensorSimple.new(ARGV[1].to_i)
+sensor_apertura = SensorSimple.new(ARGV[1].to_i)
 sensor_movimiento = SensorMovimiento.new(ARGV[2].to_i)
 sensor_camara = SensorSimple.new(ARGV[3].to_i)
 
-propiedad = Departamento.new(sensor_puerta, sensor_movimiento, sensor_camara)
+propiedad = Departamento.new(sensor_apertura, sensor_movimiento, sensor_camara)
 puts "Propiedad #{propiedad.estado}"
--- app_alarma/spec/departamento_spec.rb
+++ app_alarma/spec/departamento_spec.rb
@@ -2,18 +2,18 @@ require 'spec_helper'
 
 describe 'Departamento' do
 
-  it 'Departamento con sensor de puerta activo esta comprometido' do
-    sensor_puerta = SensorSimple.new(1)
+  it 'Departamento con sensor de apertura activo esta comprometido' do
+    sensor_apertura = SensorSimple.new(1)
     sensor_movimiento = SensorMovimiento.new(3)
     sensor_camara = SensorSimple.new(0)
-    departamento = Departamento.new(sensor_puerta, sensor_movimiento, sensor_camara)
+    departamento = Departamento.new(sensor_apertura, sensor_movimiento, sensor_camara)
     expect(departamento.estado).to eq 'COMPROMETIDA'
   end
   it 'Departamento sin sensores activos esta asegurado' do
-    sensor_puerta = SensorSimple.new(0)
+    sensor_apertura = SensorSimple.new(0)
     sensor_movimiento = SensorMovimiento.new(2)
     sensor_camara = SensorSimple.new(0)
-    departamento = Departamento.new(sensor_puerta, sensor_movimiento, sensor_camara)
+    departamento = Departamento.new(sensor_apertura, sensor_movimiento, sensor_camara)
     expect(departamento.estado).to eq 'ASEGURADA'
   end
 
