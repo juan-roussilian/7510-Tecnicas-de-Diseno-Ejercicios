827957894acd499b59e7b46ae58a265a279f9569
Juan <jroussilian@fi.uba.ar>
2023-03-26 18:43:39 -0300

    Implemento metodo estado para propiedad comercio y agrego un test unitario

--- app_alarma/model/comercio.rb
+++ app_alarma/model/comercio.rb
@@ -1,4 +1,6 @@
 class Comercio
+  MENSAJE_ESTADO_ASEGURADO = 'ASEGURADA'.freeze
+  MENSAJE_ESTADO_COMPROMETIDO = 'COMPROMETIDA'.freeze
   def initialize(sensor_apertura, sensor_movimiento, sensor_camara)
     @sensor_apertura = sensor_apertura
     @sensor_movimiento = sensor_movimiento
@@ -6,6 +8,10 @@ class Comercio
   end
 
   def estado
-    'ASEGURADA'
+    if @sensor_apertura.activo && (@sensor_movimiento.activo || @sensor_camara.activo)
+      return MENSAJE_ESTADO_COMPROMETIDO
+    end
+
+    MENSAJE_ESTADO_ASEGURADO
   end
 end
--- app_alarma/spec/comericio_spec.rb
+++ app_alarma/spec/comericio_spec.rb
@@ -8,4 +8,11 @@ describe 'Comercio' do
     comercio = Comercio.new(sensor_apertura, sensor_movimiento, sensor_camara)
     expect(comercio.estado).to eq 'ASEGURADA'
   end
+  it 'Comercio con sensor de apertura activo y camara activo esta comprometido' do
+    sensor_apertura = SensorSimple.new(1)
+    sensor_movimiento = SensorMovimiento.new(0)
+    sensor_camara = SensorSimple.new(1)
+    comercio = Comercio.new(sensor_apertura, sensor_movimiento, sensor_camara)
+    expect(comercio.estado).to eq 'COMPROMETIDA'
+  end
 end
