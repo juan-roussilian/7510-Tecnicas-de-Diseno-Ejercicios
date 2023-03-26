4f0a166705febea472949dfa249eae2489221e15
Juan <jroussilian@fi.uba.ar>
2023-03-26 14:59:42 -0300

    Agrego tests unitarios que prueban si el sensor de puerta se activa correctamente

--- app_alarma/spec/sensor_puerta_spec.rb
+++ app_alarma/spec/sensor_puerta_spec.rb
@@ -1,8 +1,14 @@
 require 'spec_helper'
 
 describe 'SensorPuerta' do
-  it 'sensor de puerta con 0 no esta activo' do
+
+  it 'sensor de puerta con valor 0 no esta activo' do
     sensor = SensorPuerta.new(0)
     expect(sensor.activo).to eq false
+  end
+
+  it 'sensor de puerta con valor 1 esta activo' do
+    sensor = SensorPuerta.new(1)
+    expect(sensor.activo).to eq true
   end
 end
