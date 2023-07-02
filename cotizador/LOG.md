8e3c45e23623eabf1eaaa35eac42653674cd58a2
Juan <jroussilian@fi.uba.ar>
2023-07-02 15:29:56 -0300

    test: Agrego otro test de aceptacion sobre cotizacion de auto con distinto kilometraje y cilindrada

--- cotizador/LOG.md
+++ cotizador/LOG.md
@@ -4,3 +4,4 @@
 - Escribo clase cotizador con test unitario sobre metodo cotizacion
 - Creo clases impuesto y auto vacias para que pase el test de cotizador
 - Agrego logica en app para que pase el segundo test de aceptacion
+- Agrego tercer test de aceptacion para cotizacion de auto, de manera que deba implementar el cotizador sin hardcodeo
--- cotizador/spec/app_spec.rb
+++ cotizador/spec/app_spec.rb
@@ -11,9 +11,15 @@ describe 'App' do
     comando = `ruby app.rb auto/1000/1000`
     expect(comando.strip).to eq 'ci:1 & vm:500.0'
   end
-  
+
   it 'auto con cilindrada 2000 y kilometraje 1000 cotiza  ci 2  y vm 1000.0' do
     comando = `ruby app.rb auto/2000/1000`
     expect(comando.strip).to eq 'ci:2 & vm:1000.0'
   end
+=begin
+  it 'auto con cilindrada 1000 y kilometraje 3000 cotiza  ci 1 y vm 250.0' do
+    comando = `ruby app.rb auto/1000/300000`
+    expect(comando.strip).to eq 'ci:1 & vm:250.0'
+  end
+=end
 end
